3aad3e43aa9ff1d298b24441ca2a65f1b52b8ae7
Wei Wang <wvw@google.com>
2017-11-16 22:04:28 -0800

    msm8998-common: init: remove CPUBW min freq setting
    CPU BW is set to 200Mhz in QC MTP, since on Pixel, we also adjust CPU BW
    in touch PowerHint, we can relax the setting to 100Mhz.
    
    Bug: 69271302
    Test: boot
    Change-Id: Iead3634d9c6fb0b3edc24ab0a19d62157a6a1793
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- prebuilt/vendor/bin/init.qcom.post_boot.sh
+++ prebuilt/vendor/bin/init.qcom.post_boot.sh
@@ -3819,7 +3819,6 @@ case "$target" in
         do
             echo "bw_hwmon" > $cpubw/governor
             echo 50 > $cpubw/polling_interval
-            echo 1525 > $cpubw/min_freq
             echo "3143 5859 11863 13763" > $cpubw/bw_hwmon/mbps_zones
             echo 4 > $cpubw/bw_hwmon/sample_ms
             echo 34 > $cpubw/bw_hwmon/io_percent
