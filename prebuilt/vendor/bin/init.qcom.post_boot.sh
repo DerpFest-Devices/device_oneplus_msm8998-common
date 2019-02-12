1bb7bb5d56cf0208abb9b40cc8530bf02e253460
celtare21 <celtare21@gmail.com>
2019-02-12 12:50:44 +0000

    msm8998-common: Properly set bw_hwmon
    Change-Id: I7e94ec1e094c29e373827059b9fa760a17c6d806
    Signed-off-by: celtare21 <celtare21@gmail.com>
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- prebuilt/vendor/bin/init.qcom.post_boot.sh
+++ prebuilt/vendor/bin/init.qcom.post_boot.sh
@@ -3821,25 +3821,6 @@ case "$target" in
 	# Enable Adaptive LMK denzel.chen
 	echo "18432,23040,27648,51256,150296,200640" > /sys/module/lowmemorykiller/parameters/minfree
 
-        # Enable bus-dcvs
-        for cpubw in /sys/class/devfreq/*qcom,cpubw*
-        do
-            echo "bw_hwmon" > $cpubw/governor
-            echo 50 > $cpubw/polling_interval
-            echo "3143 5859 11863 13763" > $cpubw/bw_hwmon/mbps_zones
-            echo 4 > $cpubw/bw_hwmon/sample_ms
-            echo 34 > $cpubw/bw_hwmon/io_percent
-            echo 20 > $cpubw/bw_hwmon/hist_memory
-            echo 10 > $cpubw/bw_hwmon/hyst_length
-            echo 0 > $cpubw/bw_hwmon/low_power_ceil_mbps
-            echo 34 > $cpubw/bw_hwmon/low_power_io_percent
-            echo 20 > $cpubw/bw_hwmon/low_power_delay
-            echo 0 > $cpubw/bw_hwmon/guard_band_mbps
-            echo 250 > $cpubw/bw_hwmon/up_scale
-            echo 1600 > $cpubw/bw_hwmon/idle_mbps
-        done
-
-        echo "cpufreq" > /sys/class/devfreq/soc:qcom,mincpubw/governor
 	if [ -f /sys/devices/soc0/soc_id ]; then
 		soc_id=`cat /sys/devices/soc0/soc_id`
 	else
