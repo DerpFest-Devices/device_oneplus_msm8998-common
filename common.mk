02ac3d2ce916b0fdfd81a2d0947d4ac84dc6078b
David Sn <divad.nnamtdeis@gmail.com>
2019-05-13 12:18:54 +0200

    msm8998-common: Fix pocketmode name capitalization
    Signed-off-by: David Sn <divad.nnamtdeis@gmail.com>

--- common.mk
+++ common.mk
@@ -204,7 +204,7 @@ PRODUCT_PACKAGES += \
 # Fingerprint sensor
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service \
-    OneplusPocketMode
+    OnePlusPocketMode
 
 # For android_filesystem_config.h
 PRODUCT_PACKAGES += \
