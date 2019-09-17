b0fc6ebff249869088ea75f1f0fbc39491b41f93
celtare21 <celtare21@gmail.com>
2019-09-17 22:04:36 +0200

    msm8998-common: Import Dexpreopt config from crosshatch
    Signed-off-by: celtare21 <celtare21@gmail.com>
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -208,12 +208,14 @@ OVERRIDE_RS_DRIVER:= libRSDriver_adreno.so
 # DRM
 TARGET_ENABLE_MEDIADRM_64 := true
 
-# Enable dexpreopt to speed boot time
+# Dexpreopt
 ifeq ($(HOST_OS),linux)
-  ifneq ($(TARGET_BUILD_VARIANT),eng)
-    ifeq ($(WITH_DEXPREOPT),)
-      WITH_DEXPREOPT := true
-      WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
+  ifeq ($(WITH_DEXPREOPT),)
+    WITH_DEXPREOPT := true
+    WITH_DEXPREOPT_PIC := true
+    ifneq ($(TARGET_BUILD_VARIANT),user)
+      # Retain classes.dex in APK's for non-user builds
+      DEX_PREOPT_DEFAULT := nostripping
     endif
   endif
 endif
