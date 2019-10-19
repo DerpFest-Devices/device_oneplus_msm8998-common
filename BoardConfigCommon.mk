f661194d7667462a3b6268a7e85766b0578e9963
XiNGRZ <chenxingyu92@gmail.com>
2019-10-19 17:27:10 +0800

    sdm845: Add shims for camera vstab blobs
    Change-Id: I99f10ee645ca58d101a80a5cf8e22fd98bf9b36b

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -174,6 +174,8 @@ TARGET_USES_QTI_CAMERA_DEVICE := true
 TARGET_USES_QTI_CAMERA2CLIENT := true
 BOARD_QTI_CAMERA_32BIT_ONLY := true
 TARGET_USES_MEDIA_EXTENSIONS := true
+TARGET_LD_SHIM_LIBS += /vendor/lib/libmms_hal_vstab.so|/vendor/lib/libshim_camera.so
+TARGET_LD_SHIM_LIBS += /vendor/lib/libmms_warper_vstab.so|/vendor/lib/libshim_camera.so
 
 # Charger
 BOARD_CHARGER_ENABLE_SUSPEND := true
