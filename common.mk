44c5e40f033ada286060595c106509389ba2bb0e
Karthi Kandasamy <kartka@codeaurora.org>
2019-12-12 15:28:40 +0530

    msm8998-common: Update display config to version 1.9
     upgrade display config version to 1.9
    
    Change-Id: I29586a29b2091366fa975c2721fe1e507e62a537
    Signed-off-by: c_kartka <kartka@codeaurora.org>
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- common.mk
+++ common.mk
@@ -179,7 +179,8 @@ PRODUCT_PACKAGES += \
     libvulkan
 
 PRODUCT_PACKAGES += \
-    vendor.display.config@1.0
+    vendor.display.config@1.9 \
+    vendor.display.config@1.9_vendor
 
 # Device
 PRODUCT_PACKAGES += \
