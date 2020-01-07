3a1342b06e1c4637a418521d845fbf930faa488f
NurKeinNeid <mralexman3000@gmail.com>
2020-01-07 22:59:43 +0200

    msm8998-common: Set loop max part to 8
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -63,7 +63,7 @@ BUILD_BROKEN_DUP_RULES := true
 # Kernel
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3
 BOARD_KERNEL_CMDLINE += service_locator.enable=1 swiotlb=2048
-BOARD_KERNEL_CMDLINE += loop.max_part=7
+BOARD_KERNEL_CMDLINE += loop.max_part=8
 #BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
