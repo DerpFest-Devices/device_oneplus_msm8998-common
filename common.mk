a041e70c523bb29b312b85cd679bd38e0e7a4562
Subhajeet Muhuri <kenny3fcb@gmail.com>
2020-01-21 12:25:17 +0530

    msm8998-common: Enable Pixel power HAL
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- common.mk
+++ common.mk
@@ -338,7 +338,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service-qti
+    android.hardware.power@1.3-service.pixel-libperfmgr
 
 # Low power Whitelist
 PRODUCT_COPY_FILES += \
