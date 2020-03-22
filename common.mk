52f3b2d12d1122f31a5c2ce00e66186ab9d82ace
Pranav Vashi <neobuddy89@gmail.com>
2020-03-23 00:00:00 +0100

    msm8998-common: Update manifest according to our blobs

--- common.mk
+++ common.mk
@@ -237,7 +237,8 @@ PRODUCT_PACKAGES += \
     libgeofencing \
     libgnss \
     libsensorndkbridge \
-    libwifi-hal-ctrl
+    libwifi-hal-ctrl \
+    libsynergy_loc_api
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps/apdr.conf:$(TARGET_COPY_OUT_VENDOR)/etc/apdr.conf \
@@ -497,8 +498,13 @@ PRODUCT_PACKAGES += \
     hostapd \
     hostapd_cli \
     libqsap_sdk \
-    libQWiFiSoftApCfg \
+    libwpa_client \
+    libwifi-hal-ctrl \
     libwifi-hal-qcom \
+    vendor.qti.hardware.wifi.hostapd@1.0.vendor \
+    vendor.qti.hardware.wifi.hostapd@1.1.vendor \
+    vendor.qti.hardware.wifi.supplicant@2.0.vendor \
+    vendor.qti.hardware.wifi.supplicant@2.1.vendor \
     wificond \
     wpa_supplicant \
     wpa_supplicant.conf \
