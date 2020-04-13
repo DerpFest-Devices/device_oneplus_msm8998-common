08aa07081c8da4e390e072a787d7a91942e30696
itsjoeoui <maximeyxw@gmail.com>
2020-04-13 18:11:39 -0400

    msm8998-common: Switch to Pixel power hal
    Signed-off-by: itsjoeoui <maximeyxw@gmail.com>

--- common.mk
+++ common.mk
@@ -370,7 +370,10 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service-qti
+    android.hardware.power@1.3-service.pixel-libperfmgr
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 # Ril
 PRODUCT_PACKAGES += \
