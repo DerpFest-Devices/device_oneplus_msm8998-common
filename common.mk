aeee1e57ba41cb0c6d28a644856d9158c211d7ef
Adhitya Mohan <me@adhityamohan.in>
2020-10-10 21:19:18 +0530

    msm8998-common: init: Load dalvik heap based on ram variants

--- common.mk
+++ common.mk
@@ -9,9 +9,6 @@ $(call inherit-product, vendor/oneplus/msm8998-common/msm8998-common-vendor.mk)
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
-# Setup dalvik vm configs
-$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
-
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
