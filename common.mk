5c264fe4a700534405a2f0153d07f5f6b680d24b
Adhitya Mohan <me@adhityamohan.in>
2020-10-10 21:19:18 +0530

    msm8998-common: init: Load dalvik heap based on ram variants

--- common.mk
+++ common.mk
@@ -6,9 +6,6 @@
 
 $(call inherit-product, vendor/oneplus/msm8998-common/msm8998-common-vendor.mk)
 
-# Setup dalvik vm configs
-$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
-
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
