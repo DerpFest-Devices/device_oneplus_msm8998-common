ee1f3ea94566b4afc622920bd309f44f54edbe51
ghostrider-reborn <adithya.r02@outlook.com>
2021-01-14 20:18:35 +0100

    msm8998-common: Update dexpreopt flags

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -127,10 +127,11 @@ TARGET_ENABLE_MEDIADRM_64 := true
 # Enable dexpreopt to speed boot time
 ifeq ($(HOST_OS),linux)
   ifneq ($(TARGET_BUILD_VARIANT),eng)
-    ifeq ($(WITH_DEXPREOPT),)
-      WITH_DEXPREOPT := true
-      WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
-    endif
+    WITH_DEXPREOPT := true
+    WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
+    WITH_DEXPREOPT_DEBUG_INFO := false
+    DONT_DEXPREOPT_PREBUILTS := true
+    USE_DEX2OAT_DEBUG := false
   endif
 endif
 
