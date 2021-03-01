808d80c0b0cb91b0539473f42d0016ddda286eb6
XsaviXander <22182013+XsaviXander@users.noreply.github.com>
2021-03-01 14:28:29 +0800

    msm8998-common: Enable freeform windows
    Change-Id: I94f4f6661c493a7e2606046e669f3528e5bde176
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- common.mk
+++ common.mk
@@ -205,6 +205,10 @@ PRODUCT_PACKAGES += \
     libvndfwk_detect_jni.qti \
     libvndfwk_detect_jni.qti.vendor
 
+# Freeform Multiwindow
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
+
 # Gatekeeper HAL
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \
