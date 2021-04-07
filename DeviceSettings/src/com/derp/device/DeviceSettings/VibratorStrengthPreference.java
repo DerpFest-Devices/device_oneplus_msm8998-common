d25c26d3988efd855d1cbbc26f6c645ff9debf48
enesykaya <enesykaya@gmail.com>
2021-04-07 22:05:14 +0300

    msm8998-common: Fix paths and address denials for vibrator hal

--- DeviceSettings/src/com/derp/device/DeviceSettings/VibratorStrengthPreference.java
+++ DeviceSettings/src/com/derp/device/DeviceSettings/VibratorStrengthPreference.java
@@ -41,16 +41,13 @@ public class VibratorStrengthPreference extends Preference implements
     private int mMaxValue;
     private Vibrator mVibrator;
 
-    private static final String FILE_LEVEL = "/sys/devices/virtual/timed_output/vibrator/vtg_level";
+    private static final String FILE_LEVEL = "/sys/class/leds/vibrator/vmax_mv";
     private static final long testVibrationPattern[] = {0,250};
 
     public VibratorStrengthPreference(Context context, AttributeSet attrs) {
         super(context, attrs);
-        // from drivers/platform/msm/qpnp-haptic.c
-        // #define QPNP_HAP_VMAX_MIN_MV		116
-        // #define QPNP_HAP_VMAX_MAX_MV		3596
         mMinValue = 116;
-        mMaxValue = 2088;
+        mMaxValue = 1800;
 
         mVibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
         setLayoutResource(R.layout.preference_seek_bar);
@@ -72,7 +69,7 @@ public class VibratorStrengthPreference extends Preference implements
     }
 
 	public static String getValue(Context context) {
-		return Utils.getFileValue(FILE_LEVEL, "2088");
+		return Utils.getFileValue(FILE_LEVEL, "1800");
 	}
 
 	private void setValue(String newValue, boolean withFeedback) {
@@ -90,7 +87,7 @@ public class VibratorStrengthPreference extends Preference implements
             return;
         }
 
-        String storedValue = PreferenceManager.getDefaultSharedPreferences(context).getString(DeviceSettings.KEY_VIBSTRENGTH, "2088");
+        String storedValue = PreferenceManager.getDefaultSharedPreferences(context).getString(DeviceSettings.KEY_VIBSTRENGTH, "1800");
         Utils.writeValue(FILE_LEVEL, storedValue);
     }
 
