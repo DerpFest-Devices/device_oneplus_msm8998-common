0dc2cb1a3d15ac3e030b64209fca9d4fd7388b94
Georg Veichtlbauer <georg@vware.at>
2021-09-06 17:59:23 +0200

    msm8998-common: Rename ueventd.rc
    * Bring us in line with everyone else
    
    Change-Id: I4cdde9d9b8191b0f22f236ded69d52e15d1dcaf3

--- common.mk
+++ common.mk
@@ -255,7 +255,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/init.qcom.usb.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.qcom.usb.rc \
     $(LOCAL_PATH)/rootdir/etc/init.target.devicesettings.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.target.devicesettings.rc \
     $(LOCAL_PATH)/rootdir/etc/init.radio.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.radio.sh \
-    $(LOCAL_PATH)/rootdir/etc/ueventd.rc:$(TARGET_COPY_OUT_VENDOR)/ueventd.rc
+    $(LOCAL_PATH)/rootdir/etc/ueventd.qcom.rc:$(TARGET_COPY_OUT_VENDOR)/ueventd.rc
 
 # IFAA (Fingerprint support for Alipay)
 PRODUCT_PACKAGES += \
