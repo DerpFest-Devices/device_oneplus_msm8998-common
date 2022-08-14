018aadbf9a043bb61e6ca7455e803c8dcd879065
NurKeinNeid <mralexman3000@gmail.com>
2022-08-14 12:14:19 +0430

    msm8998-common: Derp it
    Change-Id: Ie34e59947577988f75fae52d22e3f573f4b92420
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- common.mk
+++ common.mk
@@ -17,7 +17,7 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-derp
 
 PRODUCT_ENFORCE_RRO_TARGETS += *
 
