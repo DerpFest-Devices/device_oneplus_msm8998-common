7326bb6adcf6609852f2e8babb41beed5ffc00d8
Gabriel Biren <gbiren@google.com>
2023-03-27 22:33:41 +0000

    msm8998-common: Update makefile to use the AIDL Wifi Vendor HAL.
    Bug: 274964641
    Test: Pre-submit tests
    Change-Id: I4d9114377fe75b593bbfe6a59f03d8f5d607d6a1

--- common.mk
+++ common.mk
@@ -423,7 +423,7 @@ PRODUCT_PACKAGES += \
 
 # Wifi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service \
+    android.hardware.wifi-service \
     hostapd \
     hostapd_cli \
     libwifi-hal-qcom \
