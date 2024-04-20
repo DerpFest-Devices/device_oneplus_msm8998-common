cba4bfcfc51c4642b4f163dac43e99cf589c51ca
Mathieu Chartier <mathieuc@google.com>
2024-04-20 22:52:40 +0200

    msm8998-common: Speed profile services and wifi-service to reduce RAM and storage
    Change-Id: I1d907b24cb2760ddd9cc65ac4335730c8e61a0a3

--- common.mk
+++ common.mk
@@ -395,6 +395,9 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/google/pixel \
     hardware/oneplus
 
+# Speed profile services and wifi-service to reduce RAM and storage
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+
 # Tetheroffload
 PRODUCT_PACKAGES += \
     ipacm \
