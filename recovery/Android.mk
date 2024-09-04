94839b7c2691f205b2991f39e6a717dab6f776a4
Aaron Kling <webgeek1234@gmail.com>
2024-09-04 16:13:47 -0500

    msm8998-common: Edify and updater moved to deprecated-ota
    Change-Id: Id914528dd82beac200f934a791ae7317dc3c8eda

--- recovery/Android.mk
+++ recovery/Android.mk
@@ -7,10 +7,10 @@ LOCAL_CFLAGS += -DUSES_BOOTDEVICE_PATH
 endif
 
 LOCAL_C_INCLUDES := \
+    bootable/deprecated-ota/edify/include \
+    bootable/deprecated-ota/updater/include \
     bootable/recovery \
-    bootable/recovery/edify/include \
     bootable/recovery/otautil/include \
-    bootable/recovery/updater/include \
     system/libbase/include
 
 LOCAL_SRC_FILES := recovery_updater.cpp
@@ -18,4 +18,3 @@ LOCAL_MODULE := librecovery_updater_oneplus
 LOCAL_MODULE_TAGS  := optional
 
 include $(BUILD_STATIC_LIBRARY)
-
